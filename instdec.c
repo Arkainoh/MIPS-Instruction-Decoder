0c05bb8ce38ce916454287dae010da0a1315db44
Arkainoh <arkainoh@gmail.com>
2017-01-25 18:11:12 +0900

    Add aluDecoder as a function

--- instdec.c
+++ instdec.c
@@ -13,7 +13,8 @@
 #define ALUOP    0b00000000011
 
 void printBin(unsigned int num, int bit);
-int opcodeDecoder(unsigned int opcode, unsigned int* controls, char** instname);
+int opcodeDecoder(unsigned int opcode, unsigned int *controls, char **instname);
+int aluDecoder(unsigned int funct, unsigned int aluop, unsigned int *alucontrol, unsigned int *jr, char **aluopname);
 
 int main(int argc, char* argv[]) {
 
@@ -22,22 +23,25 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	int i;
-	unsigned int opcode, rs, rt, rd, imm, funct, controls;
-	char* hexstring = argv[1];
-	char* instname;
+	unsigned int opcode, rs, rt, rd, imm, funct, controls, aluop, alucontrol, jr;
+	char *hexstring = argv[1];
+	char *instname, *aluopname;
 	unsigned int inst = (unsigned int) strtoll(hexstring, NULL, 16);
 
 	printBin(inst, 32);
 
 	opcode = inst >> 26;
-	
-	if (opcodeDecoder(opcode, &controls, &instname) == 1) return 1;
-
-	puts(instname);
-	printBin(controls, 11);
-	printf("opcode: %u\n", opcode);
-
 	funct = inst << 26 >> 26;
+
+	if (opcodeDecoder(opcode, &controls, &instname) == 1) return 1;
+	
+	aluop = controls & ALUOP;
+
+	if (aluDecoder(funct, aluop, &alucontrol, &jr, &aluopname) == 1) return 1;
+
+	printf("%s (ALU op: %s)\n", instname, aluopname);
+
+	printf("opcode: %u\n", opcode);
 	printf("funct: %u\n", funct);
 
 	rs = inst << 6 >> 27;
@@ -49,7 +53,6 @@ int main(int argc, char* argv[]) {
 	rd = inst << 16 >> 27;
 	printf("rd: %u\n", rd);
 
-	if (controls & SIGNEXT) printf("haha");
 	if (controls & SIGNEXT)
 		imm = (int) inst << 16 >> 16; // sign extension
 	else 
@@ -69,7 +72,7 @@ void printBin(unsigned int num, int bit) {
   	printf("\n");
 }
 
-int opcodeDecoder(unsigned int opcode, unsigned int* controls, char** instname) {
+int opcodeDecoder(unsigned int opcode, unsigned int *controls, char **instname) {
 	// controls = {signext, shiftl16, regwrite, regdst, alusrc, branch, memwrite, memtoreg, jump, aluop} (11 bits)
 
 	switch(opcode) {
@@ -82,13 +85,17 @@ int opcodeDecoder(unsigned int opcode, unsigned int* controls, char** instname) 
 		case 0b101011: *controls = 0b10001010000; // SW
 			*instname = "SW";
 			break;
-		case 0b000101: // BNE
-		case 0b000100: *controls = 0b10000100001; // BEQ
-			*instname = "BEQ / BNE";
+		case 0b000101: *controls = 0b10000100001; // BNE
+			*instname = "BNE";
 			break;
-		case 0b001000: 
-		case 0b001001: *controls = 0b10101000000; // ADDI, ADDIU: only difference is exception
-			*instname = "ADDI / ADDIU";
+		case 0b000100: *controls = 0b10000100001; // BEQ
+			*instname = "BEQ";
+			break;
+		case 0b001000: *controls = 0b10101000000; // ADDI
+			*instname = "ADDI";
+			break;
+		case 0b001001: *controls = 0b10101000000; // ADDIU: only difference is exception
+			*instname = "ADDIU";
 			break;
 		case 0b001101: *controls = 0b00101000010; // ORI
 			*instname = "ORI";
@@ -108,4 +115,52 @@ int opcodeDecoder(unsigned int opcode, unsigned int* controls, char** instname) 
 	}
 
 	return 0;
+}
+
+int aluDecoder(unsigned int funct, unsigned int aluop, unsigned int *alucontrol, unsigned int *jr, char **aluopname) {
+
+    switch(aluop) {
+		case 0b00: *alucontrol = 0b010;  // add
+			*aluopname = "ADD";
+			break;
+		case 0b01: *alucontrol = 0b110;  // sub
+			*aluopname = "SUB";
+			break;
+		case 0b10: *alucontrol = 0b001;  // or
+			*aluopname = "OR";
+			break;
+		default:
+	      	switch(funct) { // RTYPE
+	        	case 0b100000: *alucontrol = 0b010; // ADD
+	        		*aluopname = "ADD";
+					break;
+		        case 0b100001: *alucontrol = 0b010; // ADDU: only difference is exception
+		        	*aluopname = "ADDU";
+					break;
+				case 0b100010: *alucontrol = 0b110; // SUB
+					*aluopname = "SUB";
+					break;
+				case 0b100011: *alucontrol = 0b110; // SUBU: only difference is exception
+					*aluopname = "SUBU";
+					break;
+				case 0b100100: *alucontrol = 0b000; // AND
+					*aluopname = "AND";
+					break;
+				case 0b100101: *alucontrol = 0b001; // OR
+					*aluopname = "OR";
+					break;
+				case 0b101010: *alucontrol = 0b111; // SLT
+					*aluopname = "SLT";
+					break;
+				case 0b101011: *alucontrol = 0b111; // SLTU
+					*aluopname = "SLTU";
+					break;
+				default:
+					printf("<ERROR> Unknown Instruction\n");
+					return 1;
+	        }
+    }
+
+	*jr = (funct == 0b001000) ? 1 : 0;
+
 }
