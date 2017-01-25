34898cbee08e19b0c4e9c54cefdf6bcf4ad95ffd
Arkainoh <arkainoh@gmail.com>
2017-01-25 17:15:44 +0900

    Add instruction name

--- instdec.c
+++ instdec.c
@@ -23,39 +23,52 @@ int main(int argc, char* argv[]) {
 	int i;
 	unsigned int opcode, rs, rt, rd, imm, funct, controls;
 	char* hexstring = argv[1];
+	char* instname;
 	unsigned int inst = (unsigned int) strtol(hexstring, NULL, 16);
 	printf("%d\n", (0b0010 & 2) >> 1);
 	printBin(inst, 32);
 
 	opcode = inst >> 26;
-	printf("opcode: %u\n", opcode);
-	// assign {signext, shiftl16, regwrite, regdst, alusrc, branch, memwrite, memtoreg, jump, aluop} = controls;
-	// controls (11 bits)
+	
+	// controls = {signext, shiftl16, regwrite, regdst, alusrc, branch, memwrite, memtoreg, jump, aluop} (11 bits)
+
 	switch(opcode) {
-		case 0b000000: controls = 0b00110000011; // Rtype
+		case 0b000000: controls = 0b00110000011; // R type
+			instname = "R type";
 			break;
 		case 0b100011: controls = 0b10101001000; // LW
+			instname = "LW";
 			break;
 		case 0b101011: controls = 0b10001010000; // SW
+			instname = "SW";
 			break;
 		case 0b000101: // BNE
 		case 0b000100: controls = 0b10000100001; // BEQ
+			instname = "BEQ / BNE";
 			break;
 		case 0b001000: 
 		case 0b001001: controls = 0b10101000000; // ADDI, ADDIU: only difference is exception
+			instname = "ADDI / ADDIU";
 			break;
 		case 0b001101: controls = 0b00101000010; // ORI
+			instname = "ORI";
 			break;
 		case 0b001111: controls = 0b01101000000; // LUI
+			instname = "LUI";
 			break;
 		case 0b000010: controls = 0b00000000100; // J
+			instname = "JUMP";
 			break;
-		case 0b000011: controls = 0b00100000100; // JAL - added by Inho
+		case 0b000011: controls = 0b00100000100; // JAL
+			instname = "JAL";
 			break;
 		default: 
 			printf("<ERROR> Unknown Instruction\n");
 			return 1;
 	}
+
+	printf("%s\n", instname);
+	printf("opcode: %u\n", opcode);
 
 	funct = inst << 26 >> 26;
 	printf("funct: %u\n", funct);
